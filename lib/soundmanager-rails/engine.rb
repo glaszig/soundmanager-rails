5ee6c822094da6d3caf62d20ebf9c2bd25bfb099
glaszig <glaszig@gmail.com>
2013-10-18 04:32:48 +0200

    adding swf files to config.assets.precompile to make things work with rails 4

--- lib/soundmanager-rails/engine.rb
+++ lib/soundmanager-rails/engine.rb
@@ -3,6 +3,16 @@ module Soundmanager
     ENV = ::Rails.env
     module Rails
       class Engine < ::Rails::Engine
+
+        initializer 'soundmanager.assets' do |app|
+          app.config.assets.precompile += %w(
+            swf/soundmanager2.swf
+            swf/soundmanager2_debug.swf
+            swf/soundmanager2_flash9.swf
+            swf/soundmanager2_flash9_debug.swf
+          )
+        end
+
       end
     end
   else
